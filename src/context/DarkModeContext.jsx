95ecde0a9554c7fe4efaf4bdd371ec9b0c1050c0
Rafael Souza <rafaelsprata@outlook.com>
2024-12-29 14:47:52 -0300

    refactor: melhorias no context para manter o estilo do buttonmode

--- src/context/DarkModeContext.jsx
+++ src/context/DarkModeContext.jsx
@@ -1,16 +1,34 @@
 // contexts/ModeContext.js
-import React, { createContext, useState, useContext } from 'react';
+import React, { createContext, useState, useContext, useEffect } from 'react';
 
 // Criando o contexto
 const DarkModeContext = createContext();
 
 // Criando o provedor de contexto
 export const DarkModeProvider = ({ children }) => {
-  const [mode, setMode] = useState(true); // State para alterar o button
+  // Recupera o valor do dark mode do localStorage, ou assume true como padrão
+  const [mode, setMode] = useState(() => {
+    const savedMode = localStorage.getItem('darkMode');
+    return savedMode ? JSON.parse(savedMode) : true; // Default to true (dark mode) if not found
+  });
 
+  // Função para alternar o modo
   const toggleMode = () => {
-    setMode(prevMode => !prevMode);
+    setMode((prevMode) => {
+      const newMode = !prevMode;
+      // Salva no localStorage o estado do dark mode
+      localStorage.setItem('darkMode', JSON.stringify(newMode));
+      return newMode;
+    });
   };
+
+  useEffect(() => {
+    // Restaura o estado ao recarregar a página
+    const savedMode = localStorage.getItem('darkMode');
+    if (savedMode) {
+      setMode(JSON.parse(savedMode));
+    }
+  }, []);
 
   return (
     <DarkModeContext.Provider value={{ mode, toggleMode }}>
